f13f43faad39044deb22bac9a5603277f0401feb
maremi <checkmare@gmail.com>
2024-08-28 02:48:26 +0900

    'ファイル名変更'

--- database/migrations/2024_08_15_170050_create_classes_clear_checks_table.php
+++ database/migrations/2024_08_15_170050_create_classes_clear_checks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('classes_clear_checks_tabls', function (Blueprint $table) {
+        Schema::create('classes_clear_checks', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('users_id');
             $table->unsignedBigInteger('grade_id');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('classes_clear_checks_tabls');
+        Schema::dropIfExists('classes_clear_checks');
     }
 };
