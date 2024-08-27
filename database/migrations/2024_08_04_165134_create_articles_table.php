1640d228012e258e4fdc0039bea4f39805a844a4
Mai Tokiyasu <choco-huu-kuu@hotmail.co.jp>
2024-08-27 20:42:03 +0900

    マイグレーションファイル修正

--- database/migrations/2024_08_04_165134_create_articles_table.php
+++ database/migrations/2024_08_04_165134_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('title');
             $table->dateTime('posted_date');
             $table->longtext('article_contents');
