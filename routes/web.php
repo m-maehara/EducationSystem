24cfc617e44df95d04fef0d88b66f16fb2b4fd4c
tatsumi04 <tatumi-o-04@outlook.jp>
2024-09-02 22:42:28 +0900

    バックアップ

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\CurriculumController;
+
+use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\RegisterController;
+use App\Http\Controllers\Admin\TopController;
+use App\Http\Controllers\Admin\BannerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
+});'
+*/
+
+//ユーザー画面
+Route::prefix('user')->namespace('User')->name('user.')->group(function () {
+    Route::get('/curriculum_list', 'CurriculumController@showCurriculumList')->name('show.curriculum');
+});
+
+//管理画面
+Route::prefix('user')->namespace('Admin')->name('admin.')->group(function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('show.login');
+    Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('show.register');
+    Route::get('/top', [TopController::class, 'showTop'])->name('show.top');
+    Route::get('/banner_edit', [BannerController::class, 'showBannerEdit'])->name('show.edit');
 });
